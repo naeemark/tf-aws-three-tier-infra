d57a57d633aeddd0a90c7fdd01ce7e0d7dcced67
Naeem <naeemark@gmail.com>
2023-04-05 07:48:47 +0200

    Adds Database Module #5

---
+++ blueprint/modules/database/main.tf
@@ -0,0 +1,38 @@
+
+###########################################################
+# RDS Database
+###########################################################
+
+# Create Subnet Group
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = "db-subnet-group"
+  subnet_ids = var.private_subnet_ids
+
+  tags = {
+    Owner = "ccs_it"
+  }
+}
+
+# Create RDS Instance
+resource "aws_db_instance" "rds" {
+  identifier              = "tf-tast-database"
+  instance_class          = var.instance_class
+  allocated_storage       = var.storage_allocation
+  storage_type            = var.storage_type
+  engine                  = var.db_engine
+  engine_version          = var.db_engine_version
+  name                    = var.db_name
+  username                = var.db_user_name
+  password                = var.db_password
+  skip_final_snapshot     = true
+  backup_retention_period = 0
+  apply_immediately       = true
+  multi_az                = true
+  db_subnet_group_name    = aws_db_subnet_group.db_subnet_group.name
+  vpc_security_group_ids  = var.security_group_ids
+
+  tags = {
+    Owner = "ccs_it"
+  }
+}
+
