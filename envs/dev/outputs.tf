f255c7639cb56320012a24550bf7fca80d8763a4
Naeem <naeemark@gmail.com>
2023-04-05 07:49:37 +0200

    Update Outputs

--- envs/dev/outputs.tf
+++ envs/dev/outputs.tf
@@ -2,8 +2,14 @@ output "alb_dns_uri" {
   description = "Application Load Balancer URI"
   value       = "http://${module.dev_infra.alb_dns}"
 }
+
 output "bastion_public_ip" {
   description = "public ip address for bastion"
   value       = module.dev_infra.bastion_public_ip
+}
+
+output "database_endpoint" {
+  description = "Databas connection endpoint"
+  value       = module.dev_infra.database_endpoint
 }
 
